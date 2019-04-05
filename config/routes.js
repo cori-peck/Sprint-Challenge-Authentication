77bccc12d4cf06f789206db6e0b50a6338da5d54
cori-peck <cori.peck@ymail.com>
2019-04-05 12:18:07 -0500

    login functional and token generated works to access jokes. MVP

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const Users = require('./users-model');
 
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -30,6 +30,24 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({
+        message: `Welcome ${user.username}!, have a token...`,
+        token
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json(error);
+  })
 }
 
 function getJokes(req, res) {
